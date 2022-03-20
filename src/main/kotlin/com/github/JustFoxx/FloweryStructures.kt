9a7eb002443a9eeddd06d7f838a336e925cecb92
JustFoxx <justfoxxo@outlook.com>
2022-03-20 19:19:24 +0100

    CHanged to 1.18.2 and changed modid

---
+++ src/main/kotlin/com/github/JustFoxx/FloweryStructures.kt
@@ -0,0 +1,8 @@
+package com.github.JustFoxx
+
+@Suppress("unused")
+fun init() {
+    val list = listOf("OwO", "UwU", "Flowewy!", "Magic", "Fabric > Forge", "Quilt, when?", "Fox or Umbreon")
+    println(list.random())
+}
+
