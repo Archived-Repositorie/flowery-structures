cc46a4a3271e1de782241bc7f9daf31541d5b7f3
JustFoxx <justfoxxo@outlook.com>
2022-03-21 14:04:21 +0100

    Added random output

--- src/main/kotlin/io/github/justfoxx/flowery/Flowery.kt
+++ src/main/kotlin/io/github/justfoxx/flowery/Flowery.kt
@@ -1,18 +1,19 @@
 package io.github.justfoxx.flowery
 
 import net.minecraftforge.fml.common.Mod
-import org.apache.logging.log4j.Level
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
-
 @Mod(Flowery.ID)
 object Flowery {
     const val ID = "flowery"
+    val list =
+        listOf(
+                "OwO",
+                "UwU",
+                "Flowewy!",
+                "Magic",
+                "Fabric > Forge",
+                "Quilt, when?",
+                "Fox or Umbreon"
+        )
 
-    // the logger for our mod
-    val LOGGER: Logger = LogManager.getLogger(ID)
-
-    init {
-        LOGGER.log(Level.INFO, "Hello world!")
-    }
+    println(list.random())
 }
