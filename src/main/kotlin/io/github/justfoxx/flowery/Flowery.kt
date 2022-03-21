a44a03bb48c0e454933bf6ed5c090c5134d85543
JustFoxx <justfoxxo@outlook.com>
2022-03-21 15:20:49 +0100

    Added init

--- src/main/kotlin/io/github/justfoxx/flowery/Flowery.kt
+++ src/main/kotlin/io/github/justfoxx/flowery/Flowery.kt
@@ -1,19 +1,21 @@
 package io.github.justfoxx.flowery
 
 import net.minecraftforge.fml.common.Mod
+
 @Mod(Flowery.ID)
 object Flowery {
     const val ID = "flowery"
-    val list =
-        listOf(
-                "OwO",
-                "UwU",
-                "Flowewy!",
-                "Magic",
-                "Fabric > Forge",
-                "Quilt, when?",
-                "Fox or Umbreon"
-        )
-
-    println(list.random())
+    init {
+        val list =
+                listOf(
+                        "OwO",
+                        "UwU",
+                        "Flowewy!",
+                        "Magic",
+                        "Fabric > Forge",
+                        "Quilt, when?",
+                        "Fox or Umbreon"
+                )
+        println(list.random())
+    }
 }
