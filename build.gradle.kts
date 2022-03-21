2426f3d319c5d3dba2c9cd4ca4247326bbf1ba6a
JustFoxx <justfoxxo@outlook.com>
2022-03-21 15:26:46 +0100

    Paper branch

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+plugins {
+    java
+    val kotlinVersion: String by System.getProperties()
+    kotlin("jvm").version(kotlinVersion)
+    val shadowVersion: String by System.getProperties()
+    id("com.github.johnrengelman.shadow").version(shadowVersion)
+}
+val pluginGroup: String by project
+group = pluginGroup
+val pluginVersion: String by project
+version = pluginVersion
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+dependencies {
+    val kotlinVersion: String by System.getProperties()
+    implementation(kotlin("stdlib", kotlinVersion))
+    val paperAPIVersion: String by project
+    compileOnly("io.papermc.paper", "paper-api", paperAPIVersion)
+}
+// This can be slower than manually relocating, if that's going to be an issue manually do so instead.
+val autoRelocate by tasks.register<ConfigureShadowRelocation>("configureShadowRelocation", ConfigureShadowRelocation::class) {
+    target = tasks.getByName("shadowJar") as ShadowJar?
+    val packageName = "${project.group}.${project.name.toLowerCase()}"
+    prefix = "$packageName.shaded"
+}
+tasks {
+    val javaVersion = JavaVersion.VERSION_17
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        options.release.set(javaVersion.toString().toInt())
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions { jvmTarget = javaVersion.toString() }
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+    }
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+    shadowJar {
+        archiveClassifier.set("")
+        project.configurations.implementation.get().isCanBeResolved = true
+        configurations = listOf(project.configurations.implementation.get())
+        dependsOn(autoRelocate)
+        minimize()
+    }
+    build { dependsOn(shadowJar) }
+}
---
+++ src/main/kotlin/Flowery.kt
@@ -0,0 +1,18 @@
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("UNUSED")
+class Kotlin : JavaPlugin() {
+    init {
+        val list =
+                listOf(
+                        "OwO",
+                        "UwU",
+                        "Flowewy!",
+                        "Magic",
+                        "Fabric > Forge",
+                        "Quilt, when?",
+                        "Fox or Umbreon"
+                )
+        println(list.random())
+    }
+}
