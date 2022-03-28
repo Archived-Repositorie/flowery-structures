5ae968ab6d294741ae975e74e2b801db6df6d3a4
JustFoxx <justfoxxo@outlook.com>
2022-03-28 21:54:55 +0200

    Added loading modules logging and replaced println with slf4j

--- src/main/kotlin/io/github/JustFoxx/FloweryStructures.kt
+++ src/main/kotlin/io/github/JustFoxx/FloweryStructures.kt
@@ -1,8 +1,24 @@
 package io.github.JustFoxx
 
+import net.fabricmc.loader.api.FabricLoader
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+
 @Suppress("unused")
 fun init() {
+    val logger = LoggerFactory.getLogger("FloweryStructures")
     val list = listOf("OwO", "UwU", "Flowewy!", "Magic", "Fabric > Forge", "Quilt, when?", "Fox or Umbreon")
-    println(list.random())
+    logger.info(list.random())
+    val supported = mapOf("croptopia" to "flowerycrop")
+    for(mod in supported) {
+        if (FabricLoader.getInstance().isModLoaded(mod.key) && !FabricLoader.getInstance().isModLoaded(mod.value)) {
+            logger.warn("Mod ${mod.key} is loaded but ${mod.key} support mod is not loaded")
+            continue
+        } else if(FabricLoader.getInstance().isModLoaded(mod.value)) {
+            logger.info("Support mod ${mod.value} has been loaded")
+        }
+    }
 }
 
